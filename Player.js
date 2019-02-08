a275b9b3afda01fbf20718ed4768af8e6549b3e9
AndrasSzemes <andras@szemes.eu>
2019-02-08 11:34:26 +0100

    All in if pair in hand

--- Player.js
+++ Player.js
@@ -6,18 +6,20 @@ class Player {
   static betRequest(gameState, bet) {
 
     let holeCards = [];
+    let my_stack = '';
     for (const player of gameState.players) {
       if (player.name === 'PokerMasters') {
         holeCards = player.hole_cards;
         console.log('my cards');
         console.log(holeCards);
-        console.log('my stack: ' + player.stack)
+        console.log('my stack: ' + player.stack);
+        my_stack = player.stack
       }
     }
 
     if (holeCards[0].rank === holeCards[1].rank) {
       console.log('minimum_raise given in: ' + gameState.minimum_raise);
-      bet(gameState.minimum_raise)
+      bet(my_stack)
     } else {
       console.log('Fold in');
       bet(0);
